3471e8d71016f96874057d4358d327161c772387
Heles <heles.hals@gmail.com>
2024-03-13 20:11:44 +0200

    Add project files.

---
+++ UDPChatApp/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace UDPChatApp
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.listBoxChat = new System.Windows.Forms.ListBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxChat
+            // 
+            this.listBoxChat.FormattingEnabled = true;
+            this.listBoxChat.Location = new System.Drawing.Point(12, 12);
+            this.listBoxChat.Name = "listBoxChat";
+            this.listBoxChat.Size = new System.Drawing.Size(400, 303);
+            this.listBoxChat.TabIndex = 0;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(12, 328);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(319, 20);
+            this.textBoxMessage.TabIndex = 1;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(337, 326);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(73, 358);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(258, 20);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(13, 361);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(55, 13);
+            this.labelName.TabIndex = 4;
+            this.labelName.Text = "Username";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 390);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.listBoxChat);
+            this.Name = "MainForm";
+            this.Text = "UDP Chat";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.ListBox listBoxChat;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+    }
+}
---
+++ UDPChatApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace UDPChatApp
+{
+    public partial class MainForm : Form
+    {
+        private const int serverPort = 12345;
+        private const string serverIP = "26.129.29.176";
+
+        private UdpClient client;
+        private IPEndPoint serverEndPoint;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            client = new UdpClient();
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            client.Client.Bind(new IPEndPoint(IPAddress.Any, 12345)); // Привязываем к любому доступному порту
+            Thread receiveThread = new Thread(ReceiveMessages);
+            receiveThread.Start();
+        }
+
+
+        private void ReceiveMessages()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] data = client.Receive(ref serverEndPoint);
+                    string message = Encoding.Unicode.GetString(data);
+                    Invoke(new Action(() => listBoxChat.Items.Add(message)));
+                }
+            }
+            catch (SocketException ex)
+            {
+                // Обработка исключения
+                Console.WriteLine($"Ошибка при приеме сообщения: {ex.Message}");
+            }
+        }
+
+
+        private void SendMessage(string message)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            client.Send(data, data.Length, serverEndPoint);
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxMessage.Text))
+            {
+                string userName = textBoxName.Text;
+                string message = $"{userName}: {textBoxMessage.Text}";
+                SendMessage(message);
+                textBoxMessage.Clear();
+            }
+        }
+    }
+}
